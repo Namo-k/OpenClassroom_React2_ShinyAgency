703e70a3f3001be76a4ab02e91d71e6a6d3a944c
Namo-k <namodacane@etu.u-paris.fr>
2024-04-15 11:32:58 +0200

    shiny_agency formation openclassrooom react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/';
+import Survey from './pages/Survey';
+import Error from './components/Error/';
+import Header from './components/Header';
+import ClientForm from './components/ClientForm/';
+import FreelanceForm from './components/FreelanceForm/';
+import Freelances from './pages/Freelances/';
+import Results from './pages/Results/';
+import Footer from "./components/Footer";
+import {SurveyProvider, ThemeProvider} from "./utils/context";
+import GlobalStyle from "./utils/style/GlobalStyle";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+        <ThemeProvider>
+            <SurveyProvider>
+                <GlobalStyle />
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/survey/:questionNumber" element={<Survey />} />
+                    <Route path="/results" element={<Results />} />
+                    <Route path="/freelances" element={<Freelances />} />
+                    <Route path="*" element={<Error />} />
+                </Routes>
+                <Footer />
+            </SurveyProvider>
+        </ThemeProvider>
+        {/*<GlobalStyle />*/}
+        {/*<Header/>*/}
+        {/*<Routes>*/}
+
+        {/*  <Route path="/" element={<Home />} />*/}
+        {/*  <Route path="/survey/:questionNumber" element={<Survey />}>*/}
+        {/*      <Route path="client" element={<ClientForm />} />*/}
+        {/*      <Route path="freelance" element={<FreelanceForm />} />*/}
+        {/*  </Route>*/}
+        {/*  <Route path="/freelances" element={<Freelances />} />*/}
+        {/*  <Route path="/results" element={<Results />} />*/}
+
+        {/*  <Route path="*" element={<Error />} />*/}
+        {/*</Routes>*/}
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
